eb246ff3caedd56347474b259ae02cdc101f08cf
mashingan <rahmat.d.ruffy@gmail.com>
2017-02-08 12:09:32 +0700

    first commit

---
+++ cmd.nim
@@ -0,0 +1,21 @@
+import terminal
+import strutils
+
+proc getpasswd*(prompt: string, strlen = 30, outh = stdout): string =
+  ## Shadow the input from command line console after printing the prompt
+  ## to the file handle.
+  outh.write prompt
+  result = ""
+  for i in 0..<strlen:
+    let c = getch()
+    if c in NewLines:
+      break
+    result &= c
+
+proc main() =
+  var passwd = getpasswd("Type something: ")
+  echo "\nYou typed: ", passwd
+  addQuitProc resetAttributes
+
+when isMainModule:
+  main()
