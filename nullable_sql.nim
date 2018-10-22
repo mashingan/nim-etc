921df720ef996ca4ca744288211b9eb1bd3123d6
mashingan <rahmat.d.ruffy@gmail.com>
2018-10-22 15:36:38 +0700

    enhance nullable_sql.nim

--- nullable_sql.nim
+++ nullable_sql.nim
@@ -4,6 +4,26 @@
 import options, strutils
 from sequtils import map
 
+import sugar
+
+proc find(str: string, c, within: char, start = 0): (int, bool) =
+  ## find whether the character not within specified within char
+  let
+    c1 = str.find(within, start = start)
+    c2 = str.find(within, start = c1+1)
+    pos = str.find(c, start = start)
+
+  # not within `within` chars
+  if c1 == -1 and c2 == -1 and pos != -1:
+    result = (pos, true)
+  # c in outer position
+  elif (pos > c1 and pos > c2) or (pos < c1 and pos < c2):
+    result = (pos, true)
+  # c within position
+  elif c1 < pos and pos < c2:
+    result = (c2, false)
+
+
 proc queryStr(query: string, args: varargs[Option[string]]):
     (string, seq[string])  =
   ## Transform '?' in query string according to optioned arguments given
@@ -14,13 +34,15 @@ proc queryStr(query: string, args: varargs[Option[string]]):
     prevq = -1
     builder = ""
     outargs = newseq[string]()
+    found = false
   for arg in args:
     prevq = qpos
-    # still need to make sure it's skipped when ? within ''
-    qpos = query.find('?', start = qpos+1)
-    if qpos == -1:
-      builder &= query[prevq+1 .. ^1]
-      break
+    while not found:
+      (qpos, found) = query.find('?', '\'', start = qpos+1)
+      if not found:
+        builder &= query[prevq+1 .. qpos]
+        prevq = qpos
+    found = false
     if arg.isNone:
       builder &= query[prevq+1 .. qpos].replace("?", "NULL")
       continue
@@ -33,9 +55,14 @@ proc queryStr(query: string, args: varargs[Option[string]]):
 
   (builder, outargs)
 
-let (q, args) = queryStr(
-  "INSERT INTO table_name(name, age, info) VALUES(?, ?, ?);",
-  none(string), some $19, some "info")
+var (q, args) = queryStr(
+  "INSERT INTO table_name(name, age, info, question) VALUES(?, ?, ?, 'any question?');",
+  none(string), some $19, some "is there any info?")
+echo q
+echo args
 
+(q, args) = queryStr(
+  "INSERT INTO table_name(name, age, question, info) VALUES(?, ?,'any question?', ?);",
+  none(string), some $19, none string)
 echo q
 echo args
