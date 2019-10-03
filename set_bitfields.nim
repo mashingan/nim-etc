06a95d3d6264f5bf70d3858da9939f963b700e72
mashingan <rahmat.d.ruffy@gmail.com>
2019-10-03 15:40:40 +0700

    add better version from forum

--- set_bitfields.nim
+++ set_bitfields.nim
@@ -8,6 +8,15 @@ type
   ActionType = enum
     atExec atWrite atRead
 
+# https://forum.nim-lang.org/t/5270#33119
+proc unixPermission(file: string): string =
+  let info = getFileInfo(file, followSymlink = false)
+  result &= [pcFile: '-', 'l', pcDir: 'd', 'l'][info.kind]
+  for i, p in [fpUserRead, fpUserWrite, fpUserExec,
+               fpGroupRead, fpGroupWrite, fpGroupExec,
+               fpOthersRead, fpOthersWrite, fpOthersExec]:
+    result &= (if p in info.permissions: "rwx"[i mod 3] else: '-')
+
 converter unixStyle(perm: set[FilePermission]): string =
   let permstring = fmt"{cast[uint16](perm):o}".reversed
   result = "-"
