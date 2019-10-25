086e8fc3b66ff8618b42757df2b1fcdb40db8cf4
mashingan <rahmat.d.ruffy@gmail.com>
2019-10-25 19:36:54 +0700

    sugar bindWith proc

--- writermonad_rosetta.nim
+++ writermonad_rosetta.nim
@@ -2,21 +2,19 @@
 #https://rosettacode.org/wiki/Monads/Writer_monad
 
 from math import sqrt
-from sugar import `=>`
+from sugar import `=>`, `->`
 
 type
   WriterUnit = (float, string)
   WriterBind = proc(a: WriterUnit): WriterUnit
 
-proc bindWith(f: proc(x: float): float; log: string): WriterBind =
-  result = (proc(a: WriterUnit): WriterUnit =
-    (f(a[0]), a[1] & log)
-  )
+proc bindWith(f: (x: float) -> float; log: string): WriterBind =
+  result = (a: WriterUnit) => (f(a[0]), a[1] & log)
 
 func doneWith(x: int): WriterUnit =
   (x.float, "")
 
-var
+let
   logRoot = sqrt.bindWith "obtained square root, "
   logAddOne = ((x: float) => x+1'f).bindWith "added 1, "
   logHalf = ((x: float) => x/2'f).bindWith "divided by 2, "
