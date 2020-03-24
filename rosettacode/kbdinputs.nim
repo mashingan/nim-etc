4766d856f8cced0443137f16132008683b2f89c0
mashingan <rahmat.d.ruffy@gmail.com>
2020-03-25 01:20:21 +0700

    update to keyboard/mouse input

--- rosettacode/kbdinputs.nim
+++ rosettacode/kbdinputs.nim
@@ -1,8 +1,8 @@
 # rosetta page
 #http://rosettacode.org/wiki/Simulate_input/Keyboard
-when defined(windows):
-  import winlean
-else:
+#http://rosettacode.org/wiki/Simulate_input/Mouse
+import os
+when not defined(windows):
   {.error: "not supported os".}
 
 type
@@ -13,6 +13,11 @@ type
     keKeyUp = 0x0002
     keUnicode = 0x0004
     keScanCode = 0x0008
+  MouseEvent = enum
+    meMove = 0x0001
+    meLeftDown = 0x0002
+    meLeftUp = 0x0004
+    meAbsolute = 0x8000
 
 
   MouseInput {.importc: "MOUSEINPUT".} = object
@@ -61,6 +66,12 @@ proc pressReleaseKeycode(input: var Input, code: int) =
   input.hwin.ki.wVk = code.cint
   input.pressRelease
 
+proc initMouse: Input =
+  result.`type` = clong itMouse
+  var mouse = MouseInput(mouseData: 0, dx: 0, dy: 0,
+    dwFlags: meAbsolute.culong or meMove.culong)
+  result.hwin = InputUnion(mi: mouse)
+
 proc main =
   var
     shift = initKey 0xa0 # VK_LSHIFT
@@ -79,5 +90,14 @@ proc main =
   key.pressReleaseKeycode 0x52 # r key
   key.pressReleaseKeycode 0x4c # l key
   key.pressReleaseKeycode 0x44 # d key
+
+  var mouse = initMouse()
+  mouse.hwin.mi.dx = clong 35_000
+  mouse.hwin.mi.dy = clong 35_000
+  sendInput(1, addr mouse, sizeof(mouse).cint)
+  sleep 1000
+  mouse.hwin.mi.dwFlags = meAbsolute.culong or meLeftDown.culong or
+    meLeftUp.culong
+  sendInput(1, addr mouse, sizeof(mouse).cint)
 
 main()
