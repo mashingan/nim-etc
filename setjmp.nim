356ec6679d967bfb969616e4913223d68dda8bab
mashingan <rahmat.d.ruffy@gmail.com>
2021-04-19 07:42:46 +0700

    fix setjmp for mingw-w64-gcc

--- setjmp.nim
+++ setjmp.nim
@@ -1,6 +1,12 @@
 const header = "<setjmp.h>"
 type JmpBuf* {.header: header, importc: "jmp_buf".} = object
 
+when defined(windows) and defined(gcc):
+  proc setjmp*(buf: var JmpBuf): int
+    {. header:header, importc: "__builtin_setjmp".}
+  proc longjmp*(buf: JmpBuf, ret = (-1))
+    {. header:header, importc: "__builtin_longjmp".}
 
-proc setjmp*(buf: var JmpBuf): int {.header:header, importc: "setjmp".}
-proc longjmp*(buf: JmpBuf, ret = (-1)) {.header:header, importc: "longjmp".}
+else:
+  proc setjmp*(buf: var JmpBuf): int {.header:header, importc: "setjmp".}
+  proc longjmp*(buf: JmpBuf, ret = (-1)) {.header:header, importc: "longjmp".}
