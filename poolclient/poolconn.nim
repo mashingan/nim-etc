0b0d0ee1d3e2fedb0ba6bc3fa13f0af7e603447b
mashingan <rahmat.d.ruffy@gmail.com>
2022-11-10 11:03:50 +0700

    put server as a different thread

--- poolclient/poolconn.nim
+++ poolclient/poolconn.nim
@@ -1,4 +1,5 @@
-import std/[deques, asyncdispatch, httpclient, tables, math, times]
+import std/[deques, asyncdispatch, httpclient, tables, math,
+            asynchttpserver, times, threadpool]
 
 type
   Pool = ref object
@@ -32,6 +33,17 @@ proc ops(pool: Pool, conn: AsyncHttpClient, id, count: int): Future[string] {.as
   result = await conn.getContent("http://localhost:3000/" & $count)
   pool.returnConn id
 
+proc serverthread {.thread.} =
+  var server = newAsyncHttpServer()
+  proc cb(req: Request) {.async.} =
+    echo "got request of path: ", req.url.path
+    discard sleepAsync(500) # to emulate slow operation
+    let responsetext = "Hello 異世界" & req.url.path
+    await req.respond(Http200, responsetext)
+
+  echo "serving on port 3000"
+  waitFor server.serve(Port 3000, cb)
+
 proc main {.async.} =
   let opsSize = 8
   var pool = newPool((opsSize div 2).nextPowerOfTwo)
@@ -46,6 +58,7 @@ proc main {.async.} =
 
   discard await all(clientops)
 
+spawn serverthread()
 let start = cpuTime()
 waitFor main()
 echo "ended after: ", cpuTime() - start
